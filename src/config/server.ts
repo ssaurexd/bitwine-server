1caa5fdf3c84b4a0a57b2b1e85257e814d9d4140
Aure <ssaurexd@gmail.com>
2021-10-29 23:26:45 -0500

    .

--- src/config/server.ts
+++ src/config/server.ts
@@ -38,7 +38,7 @@ class Server {
 	private initCors = () => {
 
 		this.app.use( cors({
-			origin: 'https://bitwine-client.vercel.app/',
+			origin: 'https://bitwine-client-ssaurexd-gmailcom.vercel.app/',
 			optionsSuccessStatus: 200,
 			credentials: true
 		}))
